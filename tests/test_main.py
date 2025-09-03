8cda960b2a754b329eec420ecebe5abaee506d5b
kingson4wu <kingson4wu@gmail.com>
2025-09-03 10:56:32 +0800

    fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,9 @@
+import pytest
+
 from forgeflow.core.automation import Config, run_automation
 
 
-# @pytest.mark.skip(reason="调试程序用的，忽略测试")
+@pytest.mark.skip(reason="调试程序用的，忽略测试")
 def test_main():
     cfg = Config(
         session="fastproxy_session",
